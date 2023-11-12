e403825741739b410173082766b39f858cd0b4b0
gabriela <anaislu12@gmail.com>
2023-11-12 05:43:43 +0100

    formulario

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,59 +6,37 @@ import MyNav from "./components/MyNav/MyNav";
 import Footer from "./components/Footer/Footer";
 import Products from "./screens/Products/Products";
 import Error404 from "./screens/Error404/Error404";
-
-
-
+import React, { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState(null);
 
-  function handleRegister (e){
+  function handleRegister(e) {
     e.preventDefault();
-   }
+    console.log (input);
+    // Lógica para el registro...
+  }
+
+  function handleInput(e) {
+    setInput ({ ...input, [e.target.name]: e.target.value});
+  }
+
   return (
-    
     <>
     
-        <BrowserRouter>
-        
-    <div>
-      <img src="https://media.licdn.com/dms/image/C4E0BAQE81VISLpErpg/company-logo_200_200/0/1630651895782?e=2147483647&v=beta&t=_EaSQSHwMNOiGNwkNbOS78QmhOd7419rrTis2Z4HZKo" alt="Logo de la empresa"  width="350" height="250" />
-    </div>
-    
+      <BrowserRouter>
+        <div>
+          <img src="https://media.licdn.com/dms/image/C4E0BAQE81VISLpErpg/company-logo_200_200/0/1630651895782?e=2147483647&v=beta&t=_EaSQSHwMNOiGNwkNbOS78QmhOd7419rrTis2Z4HZKo" alt="Logo de la empresa" width="350" height="250" />
+        </div>
+
         <MyNav />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home handleInput={handleInput} handleRegister={handleRegister} />} />
           <Route path="/productos" element={<Products />} />
           <Route path="/sobrenosotros" element={<AboutUs />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
-        <div className="main-container">
-      <form className="form-container" action="">
-        {/*email*/}
-
-        <section>
-          <span>Email</span>
-   
-   <input type="Email" />
-   </section>
-
-    {/*contraseña*/}
-
-    <section>
-          <span>Contraseña</span>
-   
-   <input type="Password" />
-        </section>
-        
-    {/*boton enviar*/}
-    <section>
-      <button onClick={e=> handleRegister(e)}>Registrar</button>
-    </section>
-
-      </form>
-
-    </div>
 
         <Footer />
       </BrowserRouter>
--- vite-project/src/screens/Home/Home.jsx
+++ vite-project/src/screens/Home/Home.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
+
+const Home = ({ handleInput, handleRegister }) => {
+  
   return (
-    <div>
-     
-      <Link to={"/sobrenosotros"}>
-        
-      </Link>
+    <div className="container d-flex  justify-content-center align-items-center  vh-100">
+      <div >
+        <h2 className="text-center mb-4">Registro</h2>
+        <Form onSubmit={handleRegister}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              input
+              name="Email"
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => handleInput(e)}
+            />
+            <Form.Text className="text-muted">
+              Nunca compartiremos su correo electrónico con nadie más.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              input
+              name="Contraseña"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => handleInput(e)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Recordar datos" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Registrar
+          </Button>
+        </Form>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
