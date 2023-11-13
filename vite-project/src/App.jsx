c789ec087a13ac96623d72effe83462f5c06fcf5
gabriela <anaislu12@gmail.com>
2023-11-13 06:46:31 +0100

    esto va andando

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,14 +8,30 @@ import Products from "./screens/Products/Products";
 import Error404 from "./screens/Error404/Error404";
 import React, { useState } from 'react';
 
+
+
+
 function App() {
   const [input, setInput] = useState(null);
 
   function handleRegister(e) {
     e.preventDefault();
     console.log (input);
-    // Lógica para el registro...
-  }
+
+    fetch("http://localhost:3000/user",{
+method: "POST",
+body: JSON.stringify(input),
+headers: {
+  "content-type": "application/json",
+},
+
+    }
+    )
+  
+  .then((res) => res.json())
+  .catch((error) =>console.error("error:", error))
+  .then ((response) => console.log("success:", response));
+}
 
   function handleInput(e) {
     setInput ({ ...input, [e.target.name]: e.target.value});
@@ -26,7 +42,7 @@ function App() {
     
       <BrowserRouter>
         <div>
-          <img src="https://media.licdn.com/dms/image/C4E0BAQE81VISLpErpg/company-logo_200_200/0/1630651895782?e=2147483647&v=beta&t=_EaSQSHwMNOiGNwkNbOS78QmhOd7419rrTis2Z4HZKo" alt="Logo de la empresa" width="350" height="250" />
+          <img src="https://media.licdn.com/dms/image/C4E0BAQE81VISLpErpg/company-logo_200_200/0/1630651895782?e=2147483647&v=beta&t=_EaSQSHwMNOiGNwkNbOS78QmhOd7419rrTis2Z4HZKo" alt="Logo de la empresa" width="150" centimagewith="0px" height="150" />
         </div>
 
         <MyNav />
--- vite-project/src/screens/Home/Home.jsx
+++ vite-project/src/screens/Home/Home.jsx
@@ -7,34 +7,101 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = ({ handleInput, handleRegister }) => {
   
   return (
-    <div className="container d-flex  justify-content-center align-items-center  vh-100">
+    <div className="container d-flex  justify-content-center align-items-center  vh-00">
+  
       <div >
         <h2 className="text-center mb-4">Registro</h2>
         <Form onSubmit={handleRegister}>
+               <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombres</Form.Label>
+            <Form.Control
+              input
+              name="nombres"
+              type="nombres"
+              placeholder="Enter nombres"
+              onChange={(e) => handleInput(e)}
+            
+            />
+   </Form.Group>
+
+   <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Identificacion</Form.Label>
+            <Form.Control
+              input
+              name="identificacion"
+              type="text"
+              placeholder="Enter cedula"
+              onChange={(e) => handleInput(e)}
+   
+            />
+   </Form.Group>
+
+   <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Fecha de Nacimiento</Form.Label>
+            <Form.Control
+              input
+              name="fecha de nacimiento"
+              type="date"
+              placeholder="Enter fecha de nacimiento"
+              onChange={(e) => handleInput(e)}
+           
+            />
+   </Form.Group>
+
+   <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Telefono</Form.Label>
+            <Form.Control
+              input
+              name="elefono"
+              type="text"
+              placeholder="Enter telefono"
+              onChange={(e) => handleInput(e)}
+       
+            />
+   </Form.Group>
+
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control
               input
-              name="Email"
+              name="email"
               type="email"
               placeholder="Enter email"
               onChange={(e) => handleInput(e)}
+        
+              
             />
-            <Form.Text className="text-muted">
+             <Form.Text className="text-muted">
               Nunca compartiremos su correo electrónico con nadie más.
             </Form.Text>
+            
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Contraseña</Form.Label>
             <Form.Control
               input
-              name="Contraseña"
+              name="contraseña"
               type="password"
               placeholder="Password"
               onChange={(e) => handleInput(e)}
+       
             />
           </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Rol</Form.Label>
+            <Form.Control
+              input
+              name="rol"
+              type="Rol"
+              placeholder="Enter rol"
+              onChange={(e) => handleInput(e)}
+      
+            />
+           
+   </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Recordar datos" />
@@ -44,8 +111,8 @@ const Home = ({ handleInput, handleRegister }) => {
             Registrar
           </Button>
         </Form>
-      </div>
-    </div>
+      </div>  
+       </div>
   );
 };
 
